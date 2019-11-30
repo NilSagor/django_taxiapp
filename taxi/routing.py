6c61bb38d2c2e5845e23ae6deb0a222a14875904
NilSagor <masum1201@gmail.com>
2019-11-30 16:21:06 +0800

    consumers added  and routing updated for pytest

--- taxi/routing.py
+++ taxi/routing.py
@@ -1,3 +1,13 @@
-from channels.routing import ProtocolTypeRouter 
+from channels.routing import ProtocolTypeRouter, URLRouter 
 
-application = ProtocolTypeRouter({})
+from django.urls import path
+from channels.auth import AuthMiddlewareStack
+from trips.consumers import TaxiConsumer
+
+application = ProtocolTypeRouter({
+	'websocket': AuthMiddlewareStack(
+		URLRouter([
+			path('taxi/', TaxiConsumer),
+			])
+		)
+	})

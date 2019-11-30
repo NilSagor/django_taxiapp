dcba917b10953f1a3a895274ba163fb2fa33ff97
NilSagor <masum1201@gmail.com>
2019-11-30 14:33:24 +0800

    Trip table with corresponding TripAdmin

--- trips/admin.py
+++ trips/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-from .models import User 
+from .models import User, Trip  
 
 # Register your models here.
 @admin.register(User)
@@ -11,4 +11,16 @@ class UserAdmin(DefaultUserAdmin):
  		super(UserAdmin,DefaultUserAdmin.__init__( )
 		self.arg = arg """
 	pass
-		
+
+@admin.register(Trip)	
+class TripAdmin(admin.ModelAdmin):
+	fields = (
+		'id', 'pick_up_address', 'drop_off_address', 
+		'status', 'created', 'updated',
+		)
+	list_display = (
+		'id', 'pick_up_address', 'drop_off_address', 
+		'status', 'created', 'updated',
+		)
+	list_filter = ('status',)
+	readonly_fields = ('id', 'created', 'updated',)

8bab76699113d9bb489d8162cca849193a7e4cd0
NilSagor <masum1201@gmail.com>
2019-12-02 13:13:00 +0800

    user group feature tested

--- trips/tests/test_http.py
+++ trips/tests/test_http.py
@@ -76,24 +76,29 @@ class AuthenticationTest(APITestCase):
 class HttpTripTest(APITestCase):
 
 	def setUp(self):
-		user = create_user()
-		self.client.login(username=user.username, password=PASSWORD)
+		self.user = create_user()
+		self.client.login(username=self.user.username, password=PASSWORD)
 
 
 	def test_user_can_list_trips(self):
 		trips = [
-			Trip.objects.create(pick_up_address = 'A', drop_off_address = 'B'),
-			Trip.objects.create(pick_up_address = 'B', drop_off_address = 'C')
+			Trip.objects.create(
+				pick_up_address = 'A', drop_off_address = 'B', rider = self.user),
+			Trip.objects.create(
+				pick_up_address = 'B', drop_off_address = 'C', rider = self.user),
+			Trip.objects.create(
+				pick_up_address = 'C', drop_off_address = 'D')
 		]
 
 		response = self.client.get(reverse('trip:trip_list'))
 		self.assertEqual(status.HTTP_200_OK, response.status_code)
-		exp_trip_ids = [str(trip.id) for trip in trips]
+		exp_trip_ids = [str(trip.id) for trip in trips[0:2]]
 		act_trip_ids = [trip.get('id') for trip in response.data]
 		self.assertCountEqual(exp_trip_ids, act_trip_ids)
 
 	def test_user_can_retrieve_trip_by_id(self):
-		trip = Trip.objects.create(pick_up_address = 'A', drop_off_address = 'B')
+		trip = Trip.objects.create(
+			pick_up_address = 'A', drop_off_address = 'B', rider = self.user)
 		response = self.client.get(trip.get_absolute_url())
 		self.assertEqual(status.HTTP_200_OK, response.status_code)
 		self.assertEqual(str(trip.id), response.data.get('id'))

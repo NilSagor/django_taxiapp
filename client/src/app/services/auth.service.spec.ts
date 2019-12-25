05eaa1d6544ea6cf38b1491831ffab8208b85ef9
NilSagor <masum1201@gmail.com>
2019-12-26 02:25:21 +0800

    sign up test suite added

--- client/src/app/services/auth.service.spec.ts
+++ client/src/app/services/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
+import { UserFactory } from '../testing/factories';
 
 describe('AuthService', () => {
 	let authService: AuthService;
@@ -11,8 +12,48 @@ describe('AuthService', () => {
   	providers: [AuthService]
   }));
 
+
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+});
+
+
+fdescribe('Authentication using a service', () => {
+	let authService: AuthService;
+ 	let httpMock: HttpTestingController;
+
+  	beforeEach(() => TestBed.configureTestingModule({
+	  	imports: [HttpClientTestingModule],
+	  	declarations: [],
+	  	providers: [AuthService]
+ 	});
+	 authService = TestBed.get(AuthService);
+	 HttpMock = TestBed.get(HttpTestingController);
+	});
+
+	afterEach(() =>{
+		httpMock.verify();
+	});
+
+	it('should allow a user to sign up for a new account', () =>{
+		// set up the data
+		const userData = UserFactory.create();
+		const photo = new File(['photo'], userData.photo, {type: 'image/jpeg'});
+		// Execute the function under test
+		authService.signUp(
+			userData.username,
+			userData.first_name,
+			userData.last_name,
+			'pAssw0rd!',
+			userData.group,
+			photo
+		).subscribe(user =>{
+			expect(user).toBe(userData);
+		});
+		const request = httpMock.expectOne('http://localhost:8000/api/sign_up/')
+	});
+
+
 });
